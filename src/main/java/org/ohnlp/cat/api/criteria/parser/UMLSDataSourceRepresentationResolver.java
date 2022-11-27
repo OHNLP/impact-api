2efbbcd7a42a589793cad8bc827696ba1550e9b9
Andrew Wen <2060andrew@gmail.com>
2022-11-26 23:58:25 -0600

    Provide clinical entity type as part of resolution process

--- src/main/java/org/ohnlp/cat/api/criteria/parser/UMLSDataSourceRepresentationResolver.java
+++ src/main/java/org/ohnlp/cat/api/criteria/parser/UMLSDataSourceRepresentationResolver.java
@@ -1,10 +1,12 @@
 package org.ohnlp.cat.api.criteria.parser;
 
+import org.ohnlp.cat.api.criteria.ClinicalEntityType;
+
 import java.util.Map;
 import java.util.Set;
 
 public interface UMLSDataSourceRepresentationResolver {
     void init(Map<String, Object> config);
-    Set<DataSourceRepresentation> resolveForUMLS(String umlsCUI);
+    Set<DataSourceRepresentation> resolveForUMLS(ClinicalEntityType type, String umlsCUI);
 
 }
